f5083f83f42b2aabc3a63730f4b90044d801143d
Hamzatovix <mansurhamzatovich@gmail.com>
2025-12-05 01:54:51 +0300

    Fix: Update CSP to allow images from local uploads directory

--- front/next.config.js
+++ front/next.config.js
@@ -6,7 +6,7 @@ const ContentSecurityPolicy = `
   default-src 'self';
   script-src 'self' 'unsafe-eval' 'unsafe-inline';
   style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-  img-src 'self' data: blob: https://*.public.blob.vercel-storage.com;
+  img-src 'self' data: blob: https://*.public.blob.vercel-storage.com http://localhost:3000 https://rosebotanique.store http://176.57.213.174:3000;
   font-src 'self' https://fonts.gstatic.com;
   connect-src 'self' https://*.supabase.co;
   media-src 'self';
